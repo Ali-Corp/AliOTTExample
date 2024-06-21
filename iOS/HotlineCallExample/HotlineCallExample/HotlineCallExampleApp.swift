01111282bd001cacbe7dd6d5b4df387184f01de7
Vu Ho <hohaivu@gmail.com>
2024-06-21 17:43:08 +0700

    240621 - Vu Ho - STABLE iOS Example (#2)

---
+++ iOS/HotlineCallExample/HotlineCallExample/HotlineCallExampleApp.swift
@@ -0,0 +1,40 @@
+//
+//  HotlineCallExampleApp.swift
+//  HotlineCallExample
+//
+//  Created by Vu Ho on 11/6/24.
+//
+
+import SwiftUI
+import ALIOTTHotline
+import SwiftyJSON
+
+@main
+struct HotlineCallExampleApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    @UIApplicationDelegateAdaptor var delegate: AppDelegate
+    
+    @State private var call: ALIOTTCall? = nil
+    
+    var body: some Scene {
+        WindowGroup {
+            ZStack {
+                ContentView()
+                    .onAppear {
+                        delegate.onRequestShowCallView = { call in
+                            self.call = call
+                        }
+                    }
+                if let call = call {
+                    NavigationView {
+                        CallView(call: call) {
+                            self.call = nil
+                        }
+                    }
+                }
+            }
+        }
+        .onChange(of: scenePhase) { phase in
+        }
+    }
+}
